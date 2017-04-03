d64a192371d85cc92ac01201e0e07fea31d3cfdd
Hersharan <hersharan@users.noreply.github.com>
2017-04-03 15:30:11 +0530

    fixed untracked files

--- app/js/bin/template/templates.js
+++
@@ -1,2 +0,0 @@
-angular.module('templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('header/header.tpl','');
-$templateCache.put('home/home.tpl','<div>Home</div>');}]);
