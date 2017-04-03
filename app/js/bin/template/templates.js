dfdc0c96fe08dc86fa3d527980cfa039acf6a2cc
Hersharan <hersharan.kaur@prontoitlabs.com>
2017-04-03 14:44:57 +0530

    first blood

---
+++ app/js/bin/template/templates.js
@@ -0,0 +1,2 @@
+angular.module('templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('header/header.tpl','');
+$templateCache.put('home/home.tpl','<div>Home</div>');}]);
---
+++ app/js/home/home-ctrl.js
@@ -0,0 +1,9 @@
+angular.module( 'home', [] ).config( function( $stateProvider ) {
+  $stateProvider.state( 'home', {
+    url: '/home',
+    templateUrl: 'app/js/home/home.tpl',
+    controller: 'HomeCtrl',
+  } );
+} ).controller( 'HomeCtrl', function( $scope ) {
+  console.log( 'in home ctrl' );
+} );
