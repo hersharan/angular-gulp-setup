06013eb343f836b3bb467bc0a6621461d80b45d5
Hersharan <hersharan@users.noreply.github.com>
2017-04-03 14:47:15 +0530

    app.js changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 angular.module( 'app', [ 'ui.router', 'home' ] ).config( function( $locationProvider, $urlRouterProvider ) {
   $locationProvider.html5Mode( true );
   $urlRouterProvider.otherwise( '/home' );
-} ).run( function( $state ) {
-  console.log( 'in angular' );
+} ).run( function() {
+  console.log( 'angular started' );
 } );
